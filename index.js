2f762e975a3822fae2528717d36e4538aaffc5d5
shannicec <nataliacheptoo@gmail.com>
2024-07-10 11:06:11 +0300

    Added the code for the distance

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    if (location > 42) {
+        return (location - 42)
+    }else if (location < 42) {
+        return (42 - location)
+    }else {
+        return 0
+    }
+}
+function distanceFromHqInFeet(location) {
+    return (distanceFromHqInBlocks(location) * 264)
+}
+function distanceTravelledInFeet(start, destination) {
+    return (distanceFromHqInFeet(destination) - distanceFromHqInFeet(start))
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) <= 400) {
+        return 0
+    }else if (distanceTravelledInFeet(start, destination) <= 2000) {
+        return 2.56
+    }else if (distanceTravelledInFeet(start, destination) >2000 && distanceTravelledInFeet(start, destination) <2500) {
+        return 25 
+    } else {
+        return 'cannot travel that far'
+    }
+}
